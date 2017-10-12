46dced60ed954019b23f65521475e5c9985ebf66
setID <32686336+setID@users.noreply.github.com>
2017-10-12 23:46:12 +0300

    Update canvas.js

--- src/canvas.js
+++ src/canvas.js
@@ -1,89 +1,80 @@
-class canvas {
+
+class Canvas {
 
     constructor() {
-        this.canvas = document.createElement('canvas');
-        this.ctx = this.canvas.getContext("2d");
-        this.vHeadlineX = 30;
-        this.vHeadlineY = 20;
-        this.vHeadline = '';
-        this.vHeadlineSize = 'bold 24px verdana, sans-serif';
-        this.vHeadlineColor = 'pink';
-        this.vTextX = 50;
-        this.vTextY = 60;
-        this.vText = '';
-        this.vTextSize = 'bold 18px verdana, sans-serif';
-        this.vTextColor = 'black';
+        this.svg = document.getElementById('svg');
+        this.background = document.getElementById('svg-background');
     }
 
-    size(w,h) {
-        this.canvas.width = w;
-        this.canvas.height = h;
+    setBackground(c) {
+        this.background.setAttribute('fill', c);
     }
-    text(txt) {
-        this.vText = txt;
+    
+    /**
+     * Изменение размера холста
+     * @param h height
+     * @param w width
+     */
+    setCanvasSize(h,w) {
+        this.svg.setAttribute('height', h);
+        this.background.setAttribute('height', h);
+        this.svg.setAttribute('width', w);
+        this.background.setAttribute('width', w);
     }
-    textSize(s) {
-        this.ctx.font = s;
-        this.vTextSize = s;
+    
+    /**
+     * Создает хидлайн, добавляет его в холст
+     * @param x position
+     * @param y position
+     */
+    createHeadline(x,y) {
+        this.headline = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+        this.setHeadlinePosition(x, y);
+        this.svg.appendChild(this.headline);
     }
-    textPosition(x,y) {
-        this.vTextX = x;
-        this.vTextY = y;
+    
+    setHeadline(txt) {
+        this.headline.textContent = txt;
     }
-    textColor(c) {
-        this.ctx.fillStyle = c;
-        this.vTextColor = c;
+    
+    setHeadlinePosition(x,y) {
+        this.headline.setAttribute('x', x);
+        this.headline.setAttribute('y', y);
     }
-    textUpdate() {
-        this.textSize(this.vTextSize);
-        this.textColor(this.vTextColor);
-        this.ctx.fillText(this.vText, this.vTextX, this.vTextY);
+    
+    setHeadlineColor(c) {
+        this.headline.style.fill = c;
     }
-    headline(txt) {
-        this.vHeadline = txt;
+    
+    setHeadlineSize(s) {
+        this.headline.style.fontSize = s;
     }
-    headlineSize(s) {
-        this.ctx.font = s;
-        this.vHeadlineSize = s;
+    
+    /**
+     * Создает основной текст, добавляет его в холст
+     * @param x position
+     * @param y position
+     */
+    createText(x,y) {
+        this.text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+        this.setTextPosition(x, y);
+        this.svg.appendChild(this.text);
     }
-    headlinePosition(x,y) {
-        this.vHeadlineX = x;
-        this.vHeadlineY = y;
+    
+    setText(txt) {
+        this.text.textContent = txt;
     }
-    headlineColor(c) {
-        this.ctx.fillStyle = c;
-        this.vHeadlineColor = c;
+    
+    setTextPosition(x,y) {
+        this.text.setAttribute('x', x);
+        this.text.setAttribute('y', y);
     }
-    headlineUpdate() {
-        this.headlineSize(this.vHeadlineSize);
-        this.headlineColor(this.vHeadlineColor);
-        this.ctx.fillText(this.vHeadline, this.vHeadlineX, this.vHeadlineY);
+    
+    setTextColor(c) {
+        this.text.style.fill = c;
     }
-    beforeEdit() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-    }
-    afterEdit() {
-        this.headlineUpdate();
-        this.textUpdate();
-        document.getElementById('canvas').innerHTML = '';
-        document.getElementById('canvas').appendChild(this.canvas);
-        this.downloadImg();
-    }
-    edit(method, value) {
-        this.beforeEdit();
-        this[method](value);
-        this.afterEdit();
-    }
-    edit(method, value1, value2) {
-        this.beforeEdit();
-        this[method](value1,value2);
-        this.afterEdit();
-    }
-    downloadImg() {
-        /* Example link
-         * <a href="#" id="download" download="CanvasImage.png">Download</a>
-         */
-        document.getElementById('download').href = this.canvas.toDataURL("image/png");
-        document.getElementById('download').addEventListener('click', download, false);
-    }
+    
+    setTextSize(s) {
+        this.text.style.fontSize = s;
+    } 
 }
