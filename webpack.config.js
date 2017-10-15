94c452a2afb52344f599df62eb9d149351b49936
Peter Savchenko <specc.dev@gmail.com>
2017-10-15 20:05:19 +0300

    Build system added

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const webpack           = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StyleLintPlugin   = require('stylelint-webpack-plugin');
+const path              = require('path');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+
+module.exports = {
+    entry: './src/editor.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                /**
+                 * Use for all JS files loaders below
+                 * - babel-loader
+                 * - eslint-loader
+                 */
+                test: /\.js$/,
+                use : [
+                    /** Babel loader */
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [ 'env' ]
+                        },
+                    },
+                    /** ES lint For webpack build */
+                    {
+                        loader: 'eslint-loader',
+                        options: {
+                            fix: true,
+                            sourceType: 'module'
+                        }
+                    }
+                ]
+            },
+            {
+                /**
+                 * Use for all CSS files loaders below
+                 * - extract-text-webpack-plugin
+                 * - postcss-loader
+                 */
+                test: /\.css$/,
+                include: __dirname + '/src',
+                use: ExtractTextPlugin.extract([
+                    {
+                        loader: 'css-loader',
+                        options: {
+                          minimize: 1,
+                          importLoaders: 1
+                        }
+                    },
+                    'postcss-loader'
+                ])
+            }
+        ]
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeOut: 100
+    },
+    devtool: NODE_ENV === 'development' ? 'source-map' : false,
+    externals: {
+
+    },
+    plugins: [
+
+        /** Lint CSS */
+        new StyleLintPlugin({
+            context : './src/styles',
+            files : 'main.css'
+        }),
+
+        /** Минифицируем CSS и JS */
+        new webpack.optimize.UglifyJsPlugin(),
+
+        /** Block build if errors found */
+        new webpack.NoEmitOnErrorsPlugin(),
+
+        /** Extract CSS bundle */
+        new ExtractTextPlugin('bundle.css')
+    ]
+};
